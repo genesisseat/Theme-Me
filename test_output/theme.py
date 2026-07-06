614232495c23219575979a68c1104c292a979f80
genesisseat <genesisseat21@gmail.com>
2026-07-06 18:30:47 +0800

    TermuxforkGenes

---
+++ test_output/theme.py
@@ -0,0 +1,43 @@
+# Created with THEME-ME v3.1 (Yuzuki)
+import os, sys, time
+
+def run(a):
+    for n in a + '\n':
+        sys.stdout.write(n)
+        sys.stdout.flush()
+        time.sleep(0.001)
+
+logo = """  \033[38;5;214m                                                         
+      ^óTTeeOOy¾f44444ôôü&&&&™™üçùù2©w00££ÍV0±ÍÍ3u        
+    ¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥bbbbbbbbbbbbbbbbb¨    
+   ¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥bbbbbbbbbbbbbbbbbbbn   
+   ¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥bbbbbb¥¥¥bbbbbbbbbb¥¥¥   
+   ¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥I            bbbb¥¥¥¥¥¥   
+   ¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥                  bbb¥¥¥¥¥¥¥   
+   ¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥6         ¥¥¥bbb¥     ¥¥¥¥¥¥¥¥¥¥   
+   ¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥       ^bbbbbbbb¥      ¥¥¥¥¥¥¥¥¥¥¥   
+   ¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥¥      ¥bbbbbbbb¥       ¥¥¥¥¥¥¥¥¥¥FF¥   
+   ¥¥¥¥¥¥¥¥¥¥¥¥¥¥     ¥bbbbbbbbb¥      ¥¥¥¥¥¥¥¥¥¥¥FFFF¥   
+   ¥¥¥¥¥¥¥¥¥¥¥¥¥    ¥bbbbbbbbbò     k¥b¥¥¥¥¥¥¥¥¥¥FFFFF¥   
+   ¥¥¥¥¥¥¥¥¥¥¥¥   úbbbbbbbb¥      ¥b¥¥¥¥¥¥¥¥¥¥FFFFFFFF¥   
+   ¥¥¥¥¥¥¥¥¥¥¥¥   bbbbb¥¥       2bb¥¥¥¥¥¥¥¥¥¥FFFFFFFFF¥   
+   ¥¥¥¥¥¥¥¥¥¥¥b¥         ¥    ’bb¥¥¥¥¥¥¥¥¥¥FFFFFFFFFFD¥   
+   ¥¥¥¥¥¥¥¥¥¥bbbbbbbbbbb¥    ^¥¥¥¥¥¥¥¥¥¥¥FFFFFFFFFDDDD¥   
+   ¥¥¥¥¥¥¥¥bbbbbbbbbbbbñ          ¥¥¥¥¥¥FFFFFFFFFDDDDD¥   
+   ¥¥¥¥¥¥bbbbbbbbbbbbb¬   xb¥¥¥    ¥¥¥FFFFFFFFFDDDDDDD¥   
+   ¥¥¥¥¥bbbbbbbbbbbbb    ¥b¥¥¥¥¥   ¥FFFFFFFFFDDDDDDDDD¥   
+   ¥¥bbbbbbbbbbbbbb¥    ¥¥¥¥¥¥¥    ¥FFFFFFFDDDDDDDDDDD¥   
+   ¥bbbbbbbbbbbbbbb2   ¥¥¥¥¥¥¥¥   ¥FFFFFFDDDDDDDDDDDDD¥   
+   ¥bbbbbbbbbbbbbb¥   ¥¥¥¥¥¥¥ñ   ¥FFFFFFDDDDDDDDDDDDDD¥   
+   ¥bbbbbbbbbbbbbb¥  ¥¥¥¥¥¥bï   bFFFFDDDDDDDDDDDDDDDDD¥   
+   ¥bbbbbbbbbbbbb¥¥  ¥¥¥¥¥¥   ¥FFFFFDDDDDDDDDDDDDDDDDD¥   
+   ¥bbbbbbbbbbb¥¥¥¥   ¥¥¥   ž¥FFFFDDDDDDDDDDDDDDDDDDDD¥   
+   ¥bbbbbbbbb¥¥¥¥¥¥¥     1¥FFFFFDDDDDDDDDDDDDDDDDDDDññ¥   
+   ¥bbbbbbb¥¥¥¥¥¥¥¥¥¥FFFFFFFFFDDDDDDDDDDDDDDDDDDDDññññ¥   
+    bbbbb¥¥¥¥¥¥¥¥¥¥FFFFFFFFFDDDDDDDDDDDDDDDDDDDDDññññ¥    
+      ¥b¥¥¥¥¥¥¥¥¥¥FFFFFFFFFDDDDDDDDDDDDDDDDDDDDññññ¥      
+                                                          """
+
+run(logo)
+print(f"             [97m Username : [97;41m genes [0m")
+print(f"           [97m Team :[90m 213 [0m")
